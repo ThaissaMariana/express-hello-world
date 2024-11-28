95fdc84eb68a8cfd01a1caefa36cd7bedd149f7d
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-11-28 20:10:26 -0300

    Commit - 28/11
     Adicionando o endpoint /pokemons

--- app.js
+++ app.js
@@ -19,6 +19,22 @@ app.get("/meunome", (req, res) => {
 app.get("/tico", (req, res) => {
   res.send("teco");
 });
+
+app.get("/pokemons", (req, res) => {
+  const pokemons = [
+    "Pikachu",
+    "Caterpie",
+    "Pidgeotto",
+    "Bulbasaur",
+    "Charmander",
+    "Squirtle",
+    "Krabby",
+    "Muk",
+    "Tauros",
+    "Snorlax",
+  ];
+  res.json({ pokemons });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
