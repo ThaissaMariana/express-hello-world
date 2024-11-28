3e58027a9ec575494849778746d390157880010c
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-11-28 20:45:54 -0300

    Commit - 28/11 - Correção
    Corrigindo pequeno erro no o endpoint /series

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get("/pokemons", (req, res) => {
 });
 
 app.get("/series", (req, res) => {
-  const series = ["Supernatural", "Outer Banks", "Arrow"];
+  const series = ["Supernatural", "Outer Banks", "Arrow",];
   res.json({ series });
 });
 
